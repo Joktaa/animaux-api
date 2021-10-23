e98ffe0f26a69217f37a242ee8d3570a4bb6dc30
Joktaa <joris.rouziere@gmail.com>
2021-10-23 20:35:30 +0200

    add post

--- src/main/java/fr/jorisrouziere/animauxapi/controller/AnimauxController.java
+++ src/main/java/fr/jorisrouziere/animauxapi/controller/AnimauxController.java
@@ -4,7 +4,8 @@ import fr.jorisrouziere.animauxapi.model.Animal;
 import fr.jorisrouziere.animauxapi.service.AnimauxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class AnimauxController {
     @GetMapping("/animaux")
     public Iterable<Animal> getAnimaux() {
         return animauxService.getAnimaux();
+    }
+
+    @PostMapping("/animal")
+    public Animal addAnimal(@RequestBody Animal animal) {
+        return animal;
     }
 }
