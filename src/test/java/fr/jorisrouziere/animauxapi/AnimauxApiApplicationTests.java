0c13b45ba3b751d9a26b2bea91c894071c295b9c
Joktaa <joris.rouziere@gmail.com>
2021-10-27 20:21:35 +0200

    add docker-compose.yml

--- src/test/java/fr/jorisrouziere/animauxapi/AnimauxApiApplicationTests.java
+++ src/test/java/fr/jorisrouziere/animauxapi/AnimauxApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AnimauxApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    //@Test
+    //void contextLoads() {
+    //}
 
 }
