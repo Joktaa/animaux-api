3db63f5860e92726788f5c65cc0b397a17511cf1
AnthonyB05 <77343444+AnthonyB05@users.noreply.github.com>
2022-03-14 18:23:09 +0100

    readme + add test data for Heroku bdd

--- README.md
+++ README.md
@@ -21,18 +21,35 @@
 - Pour des jeux de tests utiliser data.sql en lançant les insert
 
 ##Heroku
--Pour se connecter `heroku login`
+- Pour se connecter `heroku login`
 
--Pour récuperer la branche de Heroku `heroku git:remote -a animaux-api`
+- Pour récuperer la branche de Heroku `heroku git:remote -a animaux-api`
 
--Pour activer l'api `heroku ps:scale web=1`
+- Pour activer l'api `heroku ps:scale web=1`
 
--Pour désactiver l'api `heroku ps:scale web=0`
+- Pour désactiver l'api `heroku ps:scale web=0`
 
--Pour avoir les infos `heroku info -a animaux-api`
+- Pour avoir les infos `heroku info -a animaux-api`
 
--Pour envoyer des modifications `heroku restart` -> `git push heroku master`
+- Pour envoyer des modifications `heroku restart` -> `git push heroku master`
 
+##BDD Keroku
+
+- username :b518973ea6fdde
+
+- mdp: ef4a6705
+
+- host: eu-cdbr-west-02.cleardb.net
+
+- db: heroku_9131acfccead30c
+
+
+- Connection: `mysql -u b518973ea6fdde -pef4a6705 -h eu-cdbr-west-02.cleardb.net`
+
+
+
+- Envoie des données de tests dans la BDD :
+`Get-Content [lien du dossier]\src\main\resources\data-heroku.sql | mysql -u b518973ea6fdde -pef4a6705 -h eu-cdbr-west-02.cleardb.net`
 
 
 
---
+++ src/main/resources/data-heroku.sql
@@ -0,0 +1,102 @@
+use heroku_9131acfccead30c;
+
+/*SET character_set_client = utf8;*/
+
+DROP TABLE IF EXISTS `geographie`;
+DROP TABLE IF EXISTS `reproduction`;
+DROP TABLE IF EXISTS `vie`;
+DROP TABLE IF EXISTS `sexe`;
+DROP TABLE IF EXISTS `physique`;
+DROP TABLE IF EXISTS `animaux`;
+
+CREATE TABLE `animaux`
+(
+    `a_id`               INT NOT NULL AUTO_INCREMENT,
+    `nom_commun`         VARCHAR(255) UNIQUE,
+    `genre`              VARCHAR(255),
+    `espece`             VARCHAR(255),
+    `embranchement`      VARCHAR(255),
+    `sous_embranchement` VARCHAR(255),
+    `ordre`              VARCHAR(255),
+    `uicn`               VARCHAR(255),
+    `image`              VARCHAR(255),
+    PRIMARY KEY (`a_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE `physique`
+(
+    `p_id`        INT NOT NULL AUTO_INCREMENT,
+    `id_animal`   INT NOT NULL,
+    `description` VARCHAR(255),
+    PRIMARY KEY (`p_id`),
+    FOREIGN KEY (id_animal) REFERENCES animaux (a_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE `sexe`
+(
+    `s_id`        INT NOT NULL AUTO_INCREMENT,
+    `id_animal`   INT NOT NULL,
+    `description` VARCHAR(255),
+    PRIMARY KEY (`s_id`),
+    FOREIGN KEY (id_animal) REFERENCES animaux (a_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE `vie`
+(
+    `v_id`        INT NOT NULL AUTO_INCREMENT,
+    `id_animal`   INT NOT NULL,
+    `description` VARCHAR(255),
+    PRIMARY KEY (`v_id`),
+    FOREIGN KEY (id_animal) REFERENCES animaux (a_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE `reproduction`
+(
+    `r_id`        INT NOT NULL AUTO_INCREMENT,
+    `id_animal`   INT NOT NULL,
+    `description` VARCHAR(255),
+    PRIMARY KEY (`r_id`),
+    FOREIGN KEY (id_animal) REFERENCES animaux (a_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE `geographie`
+(
+    `g_id`        INT NOT NULL AUTO_INCREMENT,
+    `id_animal`   INT NOT NULL,
+    `description` VARCHAR(255),
+    PRIMARY KEY (`g_id`),
+    FOREIGN KEY (id_animal) REFERENCES animaux (a_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO `animaux` (`nom_commun`, `genre`, `espece`, `embranchement`, `sous_embranchement`, `ordre`, `uicn`, `image`)
+VALUES ('Coelacanthe', 'Latimeria', 'Chalumnae', 'Chordes', 'Vertébré', 'Coelocanthiforme',
+    'Danger critique d extinction', 'coelacanthe.png');
+
+INSERT INTO `physique` (`id_animal`, `description`)
+VALUES (1, 'Peut dépasser les deux mètres');
+
+INSERT INTO `physique` (`id_animal`, `description`)
+VALUES (1, 'Vit 48 ans');
+
+INSERT INTO `sexe` (`id_animal`, `description`)
+VALUES (1, 'Les femelles sont plus grandes que les mâles');
+
+INSERT INTO `vie` (`id_animal`, `description`)
+VALUES (1, 'Se réfugie souvent dans des grottes');
+
+INSERT INTO `vie` (`id_animal`, `description`)
+VALUES (1, 'Chasseur nocturne');
+
+INSERT INTO `reproduction` (`id_animal`, `description`)
+VALUES (1, 'ovoviviparité');
+
+INSERT INTO `geographie` (`id_animal`, `description`)
+VALUES (1, 'Afrique Est');
+
+INSERT INTO `geographie` (`id_animal`, `description`)
+VALUES (1, 'Océan Indien');
